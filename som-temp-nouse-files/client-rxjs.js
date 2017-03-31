b126610279d60dc1d03e5f27dcb2a93bccf5f0b6
mgpeng <mgpeng@msn.com>
2017-03-31 11:18:00 +0800

    del

--- som-temp-nouse-files/client-rxjs.js
+++
@@ -1,7 +0,0 @@
-const WebSocketClient = require('websocket').client;
-const websocket=new WebSocket('ws://localhost:1337');
-
-Rx.Observable.fromEvent(websocket,'message')
-  .map(msg=>JSON.stringify(msg.data))
-  .pluck('msg')
-  .subscribe(console.log);
